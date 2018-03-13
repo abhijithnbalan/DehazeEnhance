f6f30c7d8c7ae0a7684bcb68e0a331787ce63301
pplankton <abhijithnbalan@gmail.com>
2018-03-13 14:56:24 +0530

    initial commit

---
+++ build/CMakeFiles/DehazeEnhance.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DehazeEnhance.dir/src/logger.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/capture_frame.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/view_frame.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/algorithm.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/timer.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/image_processing.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/dehaze_enhance.cpp.o"
+  "CMakeFiles/DehazeEnhance.dir/src/main.cpp.o"
+  "../bin/DehazeEnhance.pdb"
+  "../bin/DehazeEnhance"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DehazeEnhance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
